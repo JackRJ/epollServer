483b9896a55f292107db52101d8a177723d3793d
Jack <1129075520@qq.com>
2020-06-27 10:25:09 +0800

    cookie

--- src/API/DayList.cpp
+++ src/API/DayList.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-05-18 21:47:43
- * @LastEditTime: 2020-06-27 10:13:52
+ * @LastEditTime: 2020-06-27 10:25:03
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /try/API/DayListUser.cpp
@@ -161,11 +161,12 @@ int getUserItem(map<string, string>& headers_, map<string, string>& urlData, str
     // 验证 cookie 的正确性
     int userId = atoi(urlData["userId"].c_str());
     auto vec = user -> getCookie(userId);
-    if (vec.empty())
+    if (vec.size() < 3)
         return -1;
     auto pos = headers_["Cookie"].find(";");
     if (vec[2] != headers_["Cookie"].substr(0, pos))
         return -1;
+    printf("ssss\n");
     return user -> getUserItem(urlData, items, more);
 }
 
