5d920a4b8d32d94d11addab16ba6d5fe5d7da9a9
Jack <1129075520@qq.com>
2020-06-28 11:11:16 +0800

    api

--- src/API/DayList.cpp
+++ src/API/DayList.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-05-18 21:47:43
- * @LastEditTime: 2020-06-28 11:00:17
+ * @LastEditTime: 2020-06-28 11:02:55
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /try/API/DayListUser.cpp
@@ -19,7 +19,6 @@ DayListAPI::~DayListAPI()
 
 int DayListAPI::checkCooie(const int& userId, const string& cookie)
 {
-    printf("cookie : %s\n", cookie.c_str());
     auto vec = user -> getCookie(userId);
     if (vec.empty())
         return -1;
@@ -41,9 +40,7 @@ int DayListAPI::checkCooie(const int& userId, const string& cookie)
     // 转换结构体为time_t,利用difftime,计算时间差  
     // 判断时间是否在三天之内
     double diff = difftime(mktime(cur), mktime(&last));
-    printf("diff: %f\n", diff);
     auto pos = cookie.find(";");
-    printf("%s => %s\n", vec[2].c_str(), cookie.substr(0, pos).c_str());
     if (diff < 86400 * 3 && vec[2] == cookie.substr(0, pos))
         return 1;
     else if (diff >= 86400 * 3)
